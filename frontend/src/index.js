a49bd75cf2ac4e2afb79bd6f9f1ae076f6fa562b
Sravan S <sravansunkara04@gmail.com>
2025-12-12 20:47:20 +0530

    new

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-// If you are using reportWebVitals, import it here
-// import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
